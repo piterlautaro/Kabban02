d2b41babf707bc5e417774d01e7fdeaca72e2d76
Pedro <pedro@socialab.com>
2019-06-23 16:33:32 -0400

    Changes

--- app/src/main/java/cl/inacap/kabban_02/MainActivity.java
+++ app/src/main/java/cl/inacap/kabban_02/MainActivity.java
@@ -1,17 +1,15 @@
 package cl.inacap.kabban_02;
 
 import android.content.Intent;
-import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,22 +18,37 @@ import com.bumptech.glide.Glide;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import org.w3c.dom.Text;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final int SIGN_IN_REQUEST_CODE = 1;
-
     private Toolbar toolbar;
-    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        verifySession();
-        toolbar = findViewById(R.id.toolbar);
-        drawer = findViewById(R.id.drawer_layout);
-        showToolbar("Noticias",false,toolbar);
-        setNavigationDrawer(drawer,toolbar);
+        try {
+            verifySession();
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+            showToolbar("Perfil",false);
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+
+        }catch(Exception e){
+            Toast.makeText(getApplicationContext(),"Error: "+e.getMessage(),Toast.LENGTH_LONG).show();
+        }
     }
 
     /**
@@ -43,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
      * @param title (String) Título del fragment activo
      * @param upButton (boolean) Especifica si el toolbar tendrá o no UpButton (retroceso jerárquico)
      */
-    public void showToolbar(String title, boolean upButton,Toolbar toolbar){
+    public void showToolbar(String title, boolean upButton){
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
@@ -78,6 +91,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                             .getDisplayName(),
                     Toast.LENGTH_LONG)
                     .show();
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+
+            View nav_header = navigationView.getHeaderView(0);
+            TextView displayname = nav_header.findViewById(R.id.displayname);
+            ImageView userimage = nav_header.findViewById(R.id.userimage);
+
+            displayname.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+            Glide.with(nav_header.getContext()).load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl()).into(userimage);
+
+            TextView profile_name = (TextView)findViewById(R.id.profile_name);
+            CircleImageView profile_user_image = (CircleImageView)findViewById(R.id.profile_user_image);
+
+            profile_name.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+            Glide.with(this).load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl()).into(profile_user_image);
+
+            userimage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(view.getContext(),MainActivity.class));
+                }
+            });
             return true;
         }
     }
@@ -92,13 +127,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if(requestCode == SIGN_IN_REQUEST_CODE) {
             if(resultCode == RESULT_OK) {
                 Toast.makeText(this,
                         "Inicio de sesión exitosa ¡bienvenido!",
                         Toast.LENGTH_LONG)
                         .show();
+                try {
+                    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+                    navigationView.setNavigationItemSelectedListener(this);
+
+                    View nav_header = navigationView.getHeaderView(0);
+                    TextView displayname = nav_header.findViewById(R.id.displayname);
+                    ImageView userimage = nav_header.findViewById(R.id.userimage);
+
+                    displayname.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+                    Glide.with(nav_header.getContext()).load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl()).into(userimage);
+
+                    TextView profile_name = (TextView)findViewById(R.id.profile_name);
+                    CircleImageView profile_user_image = (CircleImageView)findViewById(R.id.profile_user_image);
+
+                    profile_name.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+                    Glide.with(this).load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl()).into(profile_user_image);
+
+                    userimage.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            startActivity(new Intent(view.getContext(),MainActivity.class));
+                        }
+                    });
+                }catch(Exception e){
+                    Toast.makeText(getApplicationContext(),"Error de carga de complementos: "+e.getMessage(),Toast.LENGTH_LONG).show();
+                }
             } else {
                 Toast.makeText(this,
                         "No pudimos iniciar sesión. Inténtalo de nuevo más tarde.",
@@ -111,24 +171,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
-    public void setNavigationDrawer(DrawerLayout drawer,Toolbar toolbar){
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        View nav_header = navigationView.getHeaderView(0);
-        TextView displayname = nav_header.findViewById(R.id.displayname);
-        ImageView userimage = nav_header.findViewById(R.id.userimage);
-
-        displayname.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
-        Glide.with(nav_header.getContext()).load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl()).into(userimage);
-    }
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -139,7 +181,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-
     /**
      * Agrega funcionalidades a los ítems del menú lateral
      * @param item Objeto de la clase MenuItem
@@ -148,27 +189,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        Boolean status = true;
-        switch (item.getItemId()){
-            case R.id.nav_project:
-                break;
-            case R.id.nav_groups:
-                break;
-            case R.id.nav_news:
-                break;
-            case R.id.nav_chat:
-                break;
-            case R.id.nav_singout:
-                status = false;
-                closeSession();
-                break;
+        int id = item.getItemId();
+
+        if(id == R.id.nav_project){
+
+        }else if(id == R.id.nav_groups){
+
+        }else if(id == R.id.nav_news){
+
+        }else if(id == R.id.nav_chat){
+
+        }else if(id == R.id.nav_singout){
+            closeSession();
         }
-        if(status){
-            drawer = findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
-        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-
 }
