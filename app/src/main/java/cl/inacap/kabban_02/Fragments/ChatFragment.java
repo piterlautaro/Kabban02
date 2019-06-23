c79f6389030eddf8ea3e0525e13b2d7ef830763a
Pedro <pedro@socialab.com>
2019-06-23 19:01:03 -0400

    ADD chats fragments

--- app/src/main/java/cl/inacap/kabban_02/Fragments/ChatFragment.java
+++ app/src/main/java/cl/inacap/kabban_02/Fragments/ChatFragment.java
@@ -3,15 +3,27 @@ package cl.inacap.kabban_02.Fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import cl.inacap.kabban_02.Fragments.subfragments.SubChatFragment;
+import cl.inacap.kabban_02.Fragments.subfragments.SubUsersFragment;
 import cl.inacap.kabban_02.R;
 
 
 public class ChatFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -20,8 +32,18 @@ public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        tabLayout = view.findViewById(R.id.tab_chats);
+        viewPager = view.findViewById(R.id.view_pager_chat);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getFragmentManager());
+        viewPagerAdapter.addFragment(new SubChatFragment(),"Mensajes");
+        viewPagerAdapter.addFragment(new SubUsersFragment(),"Usuarios");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        return view;
     }
 
     /**
@@ -37,5 +59,38 @@ public class ChatFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragments.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
     }
 }
